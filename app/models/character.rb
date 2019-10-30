efe8f8b64fd0ef330197543ce62094a6e1e68e1a
michaelstsnow <michael.st.snow@gmail.com>
2019-10-30 03:00:33 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -3,7 +3,8 @@ class Character < ActiveRecord::Base
   belongs_to :show
 
   def say_that_thing_you_say
-    self.actor.full_name + " always says:  " + self.catchphrase
+    self.actor
+    #self.actor.full_name + " always says:  " + self.catchphrase
   end
 
 end
