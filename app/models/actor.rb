ddff09e8ff5da9d6dac7662fafb65e9eba668b28
michaelstsnow <michael.st.snow@gmail.com>
2019-10-30 03:50:40 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,4 +2,17 @@ class Actor < ActiveRecord::Base
   has_many :characters
   has_many :shows, through: :characters
 
+  def full_name
+    name=self.first_name + " " + self.last_name
+  end
+
+  def list_roles
+    roles = []
+      for r in self.characters
+        role = r.name + " - " + r.show.name
+        roles.push(role)
+      end
+    roles
+  end
+
 end
