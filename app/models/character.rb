c4560002769ee4611898e84d7350ac3daeca18b8
michaelstsnow <michael.st.snow@gmail.com>
2019-10-30 03:09:00 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -6,7 +6,7 @@ class Character < ActiveRecord::Base
     self.name + " always says: " + self.catchphrase
   end
 
-  def build_show(name)
+  def build_show(name:)
     show=Show.new
     show.name=name
     self.show=show
