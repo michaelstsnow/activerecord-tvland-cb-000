50046d2a5c81a6ff866d85cb0d4f8bd16af8357d
michaelstsnow <michael.st.snow@gmail.com>
2019-10-30 03:09:58 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,4 +2,10 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
 
+  def build_network(name:)
+    newtork = Network.new
+    network.name=name
+    network.save
+  end
+
 end
